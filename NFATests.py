34503733e772699cf5737449685999bcbbfd0e2d
Merissa Tan <merissa030397@gmail.com>
2019-12-11 14:38:46 -0800

    Rename test file with wrong name

--- NFATests.py
+++ NFATests.py
@@ -5,42 +5,42 @@ import NFASimulator
 class NFATests(unittest.TestCase):
 
     def test_even_ones_pass(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '0011']
             result = NFASimulator.simulate_machine()
             self.assertTrue(result)
 
     def test_even_ones_fail(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '10011']
             result = NFASimulator.simulate_machine()
             self.assertFalse(result)
 
     def test_even_ones_without_ones(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '000000']
             result = NFASimulator.simulate_machine()
             self.assertTrue(result)
 
     def test_even_ones_without_zeroes_pass(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '111111']
             result = NFASimulator.simulate_machine()
             self.assertTrue(result)
 
     def test_even_ones_without_zeroes_fail(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '11111']
             result = NFASimulator.simulate_machine()
             self.assertFalse(result)
 
     def test_even_ones_with_empty_string(self):
-        with open('test_machines/odd_number_ones.txt', 'r') as test_file:
+        with open('test_machines/even_number_ones.txt', 'r') as test_file:
             sys.stdin = test_file
             sys.argv = ['', '']
             result = NFASimulator.simulate_machine()
